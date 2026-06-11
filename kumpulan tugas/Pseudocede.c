a9fbc8ce62adf5fb1e0a5164df850f908f2c43a7
sharirayulia499-cmyk <sharirayulia499@gmail.com>
2026-06-11 16:22:42 +0700

    Add pseudocode for determining graduation status

---
+++ kumpulan tugas/Pseudocede.c
@@ -0,0 +1,12 @@
+PSEUDOCODE
+Algoritma Menentukan_Kelulusan
+{menentukan suatu nilai >50 termasuk anda lulus atau anda gagal}
+Kamus
+Nilai : integer
+Deskripsi
+Read (nilai)
+If nilai > 50 then
+Write (“anda lulus”)
+Else
+Write (“anda gagal”)
+Endif 
